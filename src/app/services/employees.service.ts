9982b713c80a7e230781e2b1a464d2532120ebbc
Allen <tamarawtechnologies@gmail.com>
2019-03-15 00:29:15 +0800

    m

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -3,6 +3,7 @@ import {Employee} from '../models/employees';
 import {Observable, of} from 'rxjs';
 import {MessagesService} from './messages.service';
 import {HttpClient} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +20,10 @@ export class EmployeesService {
 
   getEmployees(): Observable<Employee[]> {
     this.log('fetched employees');
-    return this.httpClient.get<Employee[]>(this.URL);
+    return this.httpClient.get<Employee[]>(this.URL)
+      .pipe(
+        catchError(this.handleError('getEmployees', []))
+      );
   }
 
   getEmployee(id: string): Observable<Employee> {
@@ -29,5 +33,25 @@ export class EmployeesService {
 
   private log(message: string): void {
     this.messageService.add(`EmployeeService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
